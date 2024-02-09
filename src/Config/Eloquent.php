34ab515564b0f514e3192902b7b6b4a65d418980
Hamid Ghorashi <h.ghorashi@gmail.com>
2024-02-09 18:00:54 +0400

    Create a config file

---
+++ src/Config/Eloquent.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+ |----------------------------------------------------------------------------------------
+ | Custom Library Configuration
+ |----------------------------------------------------------------------------------------
+ | This configuration file is used to define settings for the Eloquent KeyGen lib library.
+ | It provides the ability to configure the primary key generator class.
+ |
+ */
+
+return [
+
+    'customModel' => [
+        /*
+         |------------------------------------------------------------
+         | Specify the class to be used as the primary key generator.
+         |------------------------------------------------------------
+         */
+
+        'generator' => \Jetcod\Eloquent\Facades\PrimaryKeyGenerator::class,
+    ],
+];
