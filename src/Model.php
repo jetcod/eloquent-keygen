b01e5fe855ccdc0aee290e8047d984cd9511aad5
Hamid Ghorashi <h.ghorashi@gmail.com>
2024-02-17 14:44:35 +0400

    ability to disable snowflake id generator

--- src/Model.php
+++ src/Model.php
@@ -12,12 +12,17 @@ class Model extends EloquentModel
         parent::boot();
 
         static::saving(function ($model) {
-            if (!$model->getKey()) {
+            if ($this->snowflake() && !$model->getKey()) {
                 $model->setIncrementing(false);
                 $keyName = $model->getKeyName();
                 $id      = PrimaryKeyGenerator::generate();
                 $model->setAttribute($keyName, $id);
             }
         });
+    }
+
+    protected function snowflake(): bool
+    {
+        return true;
     }
 }
